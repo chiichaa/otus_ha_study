1d97ed423bb49f0410c6e3286e76fa8eb6b6126c
chudakov ilia <chudakovii@gmail.com>
2023-12-03 13:48:33 +0300

    init commit

---
+++ src/main/java/otus/highload/socialnetwork/OtusHighloadStudyApplication.java
@@ -0,0 +1,13 @@
+package otus.highload.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusHighloadStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusHighloadStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/otus/highload/socialnetwork/OtusHighloadStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package otus.highload.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusHighloadStudyApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
